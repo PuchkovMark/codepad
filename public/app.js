6301034e855bb057579093f972c55716ed3a2df8
PuckovMark <mark.puchkov19@gmail.com>
2020-07-17 15:18:18 +0300

    v1.0

--- public/app.js
+++ public/app.js
@@ -1,34 +1,31 @@
 $(document).ready(function() {
-    const p = $(".pan1");
-    const d = $(".pan2");
-    const r = $("#resize");
+    var p = $(".pan1");
+    var d = $(".pan2");
+    var r = $("#resize");
 
-
-    let curr_width = p.width()
-    console.log(curr_width)
-    let unlock = false;
+    var curr_width = p.width()
+    var unlock = false;
 
     $(document).mousemove(function(e) {
-        let change = Number(curr_width + e.clientX);
+        var change = curr_width + (e.clientX - curr_width);
 
-        //console.log(change)
         if(unlock) {
-            if(change > 199) {
+            if(change > 299) {
                 $("#debug").text(e.clientX + " resize");
                 p.css("width", change);
-                //d.css("margin-left", e.clientX);
+                d.css("margin-left", change);
             }
             else {
-                //p.css("width", 200);
-                //d.css("margin-left", 200);
+                p.css("width", 300);
+                d.css("margin-left", 300);
             }
         }
     });
 
     r.mousedown(function(e) {
-        //curr_width = p.width();
+        curr_width = p.width();
         unlock = true;
-        r.css("background-color", "rgba(0, 0, 0, 0.2)");
+        r.css("background-color", "rgba(0, 0, 0, 0.3)");
     });
 
     $(document).mousedown(function(e) {
@@ -40,6 +37,6 @@ $(document).ready(function() {
     $(document).mouseup(function(e) {
         unlock = false;
         $("#debug").text("");
-        r.css("background-color", "rgba(0, 0, 0, 0.1)");
+        r.css("background-color", "rgba(0, 0, 0, 0.2)");
     });
 });
